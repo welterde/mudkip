a130dbb1271238f9ef4af200ca9bf21356f33084
jim teeuwen <jimteeuwen@gmail.com>
2010-10-12 21:40:51 +0200

    Also change client config to suit ini-parser changes.

--- client/config.go
+++ client/config.go
@@ -30,17 +30,18 @@ func (this *Config) Load(file string) (err os.Error) {
 
 func (this *Config) Save(file string) (err os.Error) {
 	cfg := ini.NewConfig()
-	cfg.AddComment("net", "Address should be in the format ip:port. It can be in")
-	cfg.AddComment("net", "IPv4 and IPv6 format. IPv6 address should be encased")
-	cfg.AddComment("net", "in brackets. For example:")
-	cfg.AddComment("net", "  address = 127.0.0.1:54321")
-	cfg.AddComment("net", "  address = [::1]:54321")
-	cfg.AddComment("net", "  address = :54321")
-	cfg.AddComment("net", "")
-	cfg.AddComment("net", "acceptinvalidcert determines if we are willing to accept self-signed")
-	cfg.AddComment("net", "certificates or not. These are not verified by an official CA, but setting")
-	cfg.AddComment("net", "this to true is good enough for debugging, or when you trust the source")
-	cfg.AddComment("net", "implicitely.")
+	cfg.AddComment("net",
+		`Address should be in the format ip:port. It can be in
+IPv4 and IPv6 format. IPv6 address should be encased
+in brackets. For example:
+  address = 127.0.0.1:54321
+  address = [::1]:54321
+  address = :54321
+
+acceptinvalidcert determines if we are willing to accept self-signed
+certificates or not. These are not verified by an official CA, but setting
+this to true is good enough for debugging, or when you trust the source
+implicitely.`)
 
 	cfg.Set("net", "address", this.Server)
 	cfg.Set("net", "secure", this.Secure)
