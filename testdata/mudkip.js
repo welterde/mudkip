797ff85a36a104b46ad4c58cb37e9c2f133c9130
jim teeuwen <jimteeuwen@gmail.com>
2010-10-15 20:17:07 +0200

    Added extra zone to mudkip.js

--- testdata/mudkip.js
+++ testdata/mudkip.js
@@ -1,15 +1,22 @@
-{
-	"name": "Mudkipia",
+{	"name": "Mudkipia",
 	"description": "This is the magical land of mudkipz and unicorns.",
 	"logo": "",
 	"zones": [
-		{
+		{	"id": 1,
 			"default": true,
 			"name": "Seaside Tavern",
 			"description": "Welcome area for tourists and would be adventurers. Feel free to hang around and chat.\n\nWhen you are ready to start hunting for the elusive unicorn, please speak to the hunting overseer in the corner to get your hunting license sorted.",
 			"lighting": "dark, candle lit with a shimmering open fire in the northern corner",
 			"smell": "of beer and stale bread",
-			"exits": ["south"]
+			"exits": [
+				["s", 2]
+			]
+		},
+		{	"id": 2,
+			"name": "Seaside Courtyard",
+			"exits": [
+				["n", 1]
+			]
 		}
 	],
 	"characters": [],
