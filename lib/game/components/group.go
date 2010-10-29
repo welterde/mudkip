c77a02ed42f7625c60501ae20bec2538221546b1
jim teeuwen <jimteeuwen@gmail.com>
2010-10-29 18:46:24 +0800

    Added proper table creation code to sqlite store

--- lib/game/components/group.go
+++ lib/game/components/group.go
@@ -5,25 +5,8 @@ type Group struct {
 	Id          int64
 	Name        string
 	Description string
-	Members     []int
 }
 
 func NewGroup() *Group {
-	v := new(Group)
-	v.Members = make([]int, 0, 32)
-	return v
-}
-
-// Add a new member
-func (this *Group) AddMember(id int) {
-	sz := len(this.Members)
-
-	if sz >= cap(this.Members) {
-		cp := make([]int, sz, sz+32)
-		copy(cp, this.Members)
-		this.Members = cp
-	}
-
-	this.Members = this.Members[0 : sz+1]
-	this.Members[sz] = id
+	return new(Group)
 }
